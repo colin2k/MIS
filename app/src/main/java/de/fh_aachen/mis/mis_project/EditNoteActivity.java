e2e0e0cecd9662455516552b2f8c4db83e790477
Benedikt Bauer <info@sam-song.info>
2015-06-09 22:51:54 +0200

    Take Photos from edit note screen

--- app/src/main/java/de/fh_aachen/mis/mis_project/EditNoteActivity.java
+++ app/src/main/java/de/fh_aachen/mis/mis_project/EditNoteActivity.java
@@ -8,7 +8,12 @@ import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,6 +22,7 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -24,6 +30,8 @@ import android.widget.TimePicker;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -49,6 +57,7 @@ public class EditNoteActivity extends Activity {
     String remind_me_datetime;
     TextView txtLocation;
     Button btnPlacePicker;
+    Button btnPhoto;
 
     private LatLng location;
     private Spinner prioSpinner;
@@ -59,7 +68,47 @@ public class EditNoteActivity extends Activity {
     private SimpleDateFormat dateFormatter;
     private SimpleDateFormat timeFormatter;
 
+    private ImageView mImageView;
+
     public static int REQUEST_CODE = 811512;
+
+    private String mCurrentPhotoPath;
+
+    private static final String JPEG_FILE_PREFIX = "IMG_";
+    private static final String JPEG_FILE_SUFFIX = ".jpg";
+
+    /* Photo album for this application */
+    private String getAlbumName() {
+        return getString(R.string.album_name);
+    }
+
+    private File getAlbumDir() {
+        File storageDir = null;
+
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+
+            storageDir = new File (
+                    Environment.getExternalStorageDirectory()
+                            + "dcim"
+                            + getAlbumName()
+            );
+
+            if (storageDir != null) {
+                if (! storageDir.mkdirs()) {
+                    if (! storageDir.exists()){
+                        Log.d("CameraSample", "failed to create directory");
+                        return null;
+                    }
+                }
+            }
+
+        } else {
+            Log.v(getString(R.string.app_name), "External storage is not mounted READ/WRITE.");
+        }
+
+        return storageDir;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,6 +161,29 @@ public class EditNoteActivity extends Activity {
                 startActivityForResult(intent, 2);
             }
         });
+
+        btnPhoto = (Button) findViewById(R.id.btnPhoto);
+        btnPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("PhotoBtn", "Clicked");
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    // Create an image file name
+                    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                    String imageFileName = JPEG_FILE_PREFIX + timeStamp + "_";
+                    File albumF = getAlbumDir();
+                    try {
+                        File imageF = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX, albumF);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imageF));
+                    } catch (IOException e) {
+                        Log.e("PhotoBtn", e.getMessage());
+                    }
+                    startActivityForResult(takePictureIntent, 666);
+                }
+            }
+        });
+
         save_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -244,18 +316,71 @@ public class EditNoteActivity extends Activity {
         super.onPause();
     }
 
+    private void setPic() {
+
+		/* There isn't enough memory to open up more than a couple camera photos */
+		/* So pre-scale the target bitmap into which the file is decoded */
+
+		/* Get the size of the ImageView */
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+		/* Get the size of the image */
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+		/* Figure out which way needs to be reduced less */
+        int scaleFactor = 1;
+        if ((targetW > 0) || (targetH > 0)) {
+            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        }
+
+		/* Set bitmap options to scale the image decode target */
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+		/* Decode the JPEG file into a Bitmap */
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+
+		/* Associate the Bitmap to the ImageView */
+        mImageView.setImageBitmap(bitmap);
+        mImageView.setVisibility(View.VISIBLE);
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1){
-            location = (LatLng)data.getExtras().get("location");
-            txtLocation.setText(location.toString());
-        }
-        if(resultCode==RESULT_OK){
-
-            /*Intent refresh = new Intent(this, AbhaengigeErinnerungenActivity.class);
-            startActivity(refresh);
-            this.finish();*/
+        switch (requestCode){
+            case 1: {
+                location = (LatLng) data.getExtras().get("location");
+                txtLocation.setText(location.toString());
+                if(resultCode==RESULT_OK){
+                    /*Intent refresh = new Intent(this, AbhaengigeErinnerungenActivity.class);
+                    startActivity(refresh);
+                    this.finish();*/
+                }
+            }
+            case 666: {
+                if (resultCode == RESULT_OK) {
+                    if (mCurrentPhotoPath != null) {
+//                        setPic();
+                        galleryAddPic();
+                        mCurrentPhotoPath = null;
+                    }
+                }
+            }
         }
     }
 }
